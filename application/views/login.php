74f2eb6d36638ba5c2667f0311e9ef38bd3c2ccf
ageesh <ageeshkg@gmail.com>
2017-11-28 12:30:47 +0530

    login php

---
+++ application/views/login.php
@@ -0,0 +1,38 @@
+<?php
+
+?>
+<div class="form-outer">
+<div class="form-left pull-left">
+<div class="parent-logo">
+<img src="assets/images/parent-logo.png" alt="">
+</div>
+</div>
+<div class="form-right pull-right">
+<h3 class="font-weight-normal">Access Your Account</h3>
+<div class="form-container">
+<form>
+<div class="form-group">
+<input type="text" id="email" name="email" required="required">
+<label for="input" class="control-label">Email address:</label><i class="bar"></i>
+</div>
+<div class="form-group">
+<input type="password" id="password" name="password" required="required">
+<label for="pwd" for="input" class="control-label">Password:</label><i class="bar"></i>
+</div>
+<div class="checkbox">
+<label>
+<input type="checkbox"/><i class="helper"></i> Remember me
+</label>
+</div>
+<div class="button-container">
+<a href="patient/patient-profile-view.html" class="btn btn-block btn-primary" type="button"><span>Login</span></a>
+</div>
+<div class="form-options">
+<a href="javascript:;" class="pull-left">Forgot Password ?</a>
+<a href="register_patient.html" class="pull-right">Create Your Account</a>
+</div>
+
+</form>
+</div>
+</div>
+</div>
