20a33e23d1d6a7ec3b323a7cacce0252a9e5f963
ageesh <ageeshkg@gmail.com>
2017-12-02 19:04:00 +0530

    Added error handler

--- index.php
+++ index.php
@@ -1,4 +1,33 @@
 <?php
+
+
+function aggaroErrorHandler($code, $message, $file, $line) {
+    
+    //Need to write the handler here, send mail or log it some error logger
+    
+    echo "Error at code ".$code." and message is ";
+    print_r($message);
+    echo "Error at file ".$file." and line no ";
+    print_r($line);
+    
+    
+}
+
+function fatalErrorShutdownHandler()
+{
+    $last_error = error_get_last();
+    if ($last_error['type'] === E_ERROR) {
+        // fatal error
+        aggaroErrorHandler(E_ERROR, $last_error['message'], $last_error['file'], $last_error['line']);
+    }
+}
+
+
+set_error_handler('aggaroErrorHandler');
+register_shutdown_function('fatalErrorShutdownHandler');
+
+
+
 require "framework/core/AgarroFramework.php";
 
 AgarroFramework::run( $_GET['url']);
